3b6e18f317c59e41cbb907f6359e58af71b97017
leo-damenace <leogao2155@gmail.com>
2026-03-27 20:29:06 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -6,7 +6,7 @@
 const COLOURS = {
   white:  { hex: "#f0f0f0", label: "White"  },
   yellow: { hex: "#ffd200", label: "Yellow" },
-  red:    { hex: "#c41e1e", label: "Red"    },
+  red:    { hex: "#c41e1e", label: "Red"    }, 
   orange: { hex: "#ff6400", label: "Orange" },
   blue:   { hex: "#0046c8", label: "Blue"   },
   green:  { hex: "#009b2d", label: "Green"  },
