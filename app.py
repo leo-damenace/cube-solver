c79aca77676b4e123a19e6daa189363fed353c84
leo-damenace <leogao2155@gmail.com>
2026-04-03 13:31:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -154,3 +154,4 @@ Replace every "c" with one of the exact color names: "white", "yellow", "red", "
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+ 
